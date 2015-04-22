a13e7fe838e2ac2e5e5db0584c73790251c9d352
Wellington Silva <wsilva@usp.br>
2015-04-21 23:55:00 -0300

    configured access to redis server via env file

--- meetup-app/config/database.php
+++ meetup-app/config/database.php
@@ -115,7 +115,7 @@ return [
 		'cluster' => false,
 
 		'default' => [
-			'host'     => '127.0.0.1',
+			'host'     => env('REDIS_SERVER', '127.0.0.1'),
 			'port'     => 6379,
 			'database' => 0,
 		],
