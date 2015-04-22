f1721559457172ae265916e67f9af3192a53392f
Wellington Silva <wsilva@usp.br>
2015-04-21 23:54:14 -0300

    Created view and controllers to the new routes

---
+++ meetup-app/app/Http/Controllers/MeetupController.php
@@ -0,0 +1,25 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use \Cache;
+
+use Illuminate\Http\Request;
+
+class MeetupController extends Controller
+{
+
+	public function meetup()
+	{
+		$curtidas = Cache::get('curtidas', 0);
+		return view('meetup.index')->with('curtidas', $curtidas);
+	}
+
+	public function curtir()
+	{
+		// to be implemented
+		return "not implemented yet";
+	}
+
+}
---
+++ meetup-app/resources/views/meetup/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Meetup</title>
+</head>
+<body>
+    <div class="container">
+        <div class="content">
+            <div class="title">Bem vindo!</div>
+            <div class="likebutton">
+                {{ $curtidas }} curtida(s)
+                <a href="#">Curtir</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
