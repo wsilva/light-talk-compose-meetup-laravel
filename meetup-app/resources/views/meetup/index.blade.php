671057f3002ea8bee795f02bbca551f877f68bf1
Wellington Silva <wsilva@usp.br>
2015-04-22 00:31:27 -0300

    improving example layout

--- meetup-app/resources/views/meetup/index.blade.php
+++ meetup-app/resources/views/meetup/index.blade.php
@@ -1,16 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <style>
+    .title, .foto, .likebutton, .server
+    {
+        text-align: center;
+    }
+
+    .server
+    {
+        color: #CCC;
+    }
+    </style>
     <meta charset="UTF-8">
     <title>Meetup</title>
 </head>
 <body>
     <div class="container">
         <div class="content">
-            <div class="title">Bem vindo!</div>
+            <div class="title">
+            <h1>Relaxa, tamo em casa...</h1>
+            </div>
+            <div class="foto"><img src="/images/helena.jpg" alt=""></div>
             <div class="likebutton">
                 {{ $curtidas }} curtida(s)
+                <br>
                 <a href="#">Curtir</a>
+            </div>
+            <div class="server">
+                Servidor: {{ $container }}
             </div>
         </div>
     </div>
